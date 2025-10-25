6d3650c863986bb34af08556d63232ebc20fc5cd
Adrián Bohabén Torés <adrian.bohaben@alu.uclm.es>
2025-10-25 11:36:44 +0200

    chore: Añadida la capa de servicio. Correciones menores en la entidades.

--- src/main/java/es/uclm/library/persistencia/ReservaDAO.java
+++ src/main/java/es/uclm/library/persistencia/ReservaDAO.java
@@ -7,8 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import es.uclm.library.negocio.dominio.Reserva;
+
 
 @Repository
 public interface ReservaDAO extends JpaRepository<Reserva, Long> {
